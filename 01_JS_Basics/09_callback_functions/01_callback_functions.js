50ae5f996dffda53921985676f7e7740cf315a93
skdharmavaram <dharmavaram.shivakrishna@zensar.com>
2026-02-02 17:16:42 +0530

    callback function

--- 01_JS_Basics/09_callback_functions/01_callback_functions.js
+++ 01_JS_Basics/09_callback_functions/01_callback_functions.js
@@ -44,19 +44,3 @@ function sayBye() {
 }
 
 greet("Shiva", sayBye);
-
-// Excercise ---01----------------------------
-function processData(data, callback) {
-  console.log("Processing:", data);
-  callback();
-}
- 
-function done() {
-  console.log("Done!");
-}
- 
-processData("Test Data", done);
-
-// output is:
-// Processing: Test Data
-// Done!
